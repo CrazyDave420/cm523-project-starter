ce6aa77d5985cb3be313525d6886164b5ece1dab
Yingzhuo <yingzhzh@bu.edu>
2023-12-10 19:08:38 -0500

    map embedded

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,3 +23,63 @@ button.addEventListener('click', function() {
   });
   button.style.display = 'none';
 });
+
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: { lat: 42.3601, lng: -71.0589 }
+  });
+
+  setMarkers(map);
+}
+
+
+  const bostonVenues = [
+    ["MGM Music Hall", 42.347076, -71.094566, 1],
+    ["Fenway Park", 42.3467, -71.0972, 2],
+    ["TD Garden", 42.3662, -71.0621, 3],
+    ["Roadrunner", 42.3566, -71.1439, 4],
+    ["House of Blues", 42.3474068, -71.0959362, 5],
+    ["Paradise Rock Club", 42.3517507, -71.1195613, 6],
+    ["Agganis Arena", 42.3522233, -71.11773, 7],
+    ["Brighton Music Hall", 42.3526574, -71.1326369, 8],
+  ];
+
+
+  bostonVenues.forEach(place => {
+    const marker = new google.maps.Marker({
+      position: { lat: place[1], lng: place[2] },
+      map: map,
+      title: place[0]
+
+    });
+  });
+
+  function setMarkers(map) {
+    const image = {
+      url: 'path/to/your/custom-icon.png', // 您的自定义图标 URL
+      size: new google.maps.Size(20, 32),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(0, 32),
+    };
+  
+    const shape = {
+      coords: [1, 1, 1, 20, 18, 20, 18, 1],
+      type: 'poly',
+    };
+  
+    bostonVenues.forEach(venue => {
+      new google.maps.Marker({
+        position: { lat: venue[1], lng: venue[2] },
+        map: map,
+        icon: image,
+        shape: shape,
+        title: venue[0],
+        zIndex: venue[3],
+      });
+    });
+  }
+  
+
+window.initMap = initMap;
